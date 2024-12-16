b7cc0a8668e80016012e246d767c068a5e70d267
Andiiie <andreaalsina0@gmail.com>
2024-12-16 18:18:40 +0100

    16.12

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        // para que la barra pueda mirar a la camara (solo funciona con el ortografico)
+        transform.forward = -cam.transform.forward; 
+    }
+}
---
+++ Assets/Scripts/SistemaPatrulla.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SistemaPatrulla : MonoBehaviour
+{
+    [SerializeField] Transform ruta;
+    [SerializeField] NavMeshAgent agentEnemy;
+
+    List<Vector3> listadoPuntos = new List<Vector3>();
+    Vector3 destinoActual;
+    int indiceRutaActual; 
+    void Awake()
+    {
+        // recorro todos los puntos de mi ruta
+        foreach (Transform punto in ruta)
+        {
+            // los añado en mi lista
+            listadoPuntos.Add(punto.position);
+        }
+
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PatrullarYEsperar());
+    }
+
+
+    IEnumerator PatrullarYEsperar()
+    {
+
+        CalcularDestino(); // 1. calculas el destino
+        agentEnemy.SetDestination(destinoActual); // 2. se te marca el destino
+        yield return new WaitUntil( () => !agentEnemy.pathPending && agentEnemy.remainingDistance <= 0.2f); //esperas hasta lelgar al destino y repites
+        yield return new WaitForSeconds(Random.Range(0.5f, 1.5f)); //4. una vex llegado al punto, esperamos x tiempo en dicho punto
+        
+        
+        //agentEnemy.SetDestination(destinoActual);
+        //yield return null;
+    }
+
+    void CalcularDestino()
+    {
+        indiceRutaActual++;
+       
+        // Count para listas es igual a Length en los arrays
+        if(indiceRutaActual >= listadoPuntos.Count)
+        {
+            indiceRutaActual = 0;
+        }
+
+        destinoActual = listadoPuntos[indiceRutaActual];
+    }
+}
